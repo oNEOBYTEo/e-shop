7555d56110a16329f048cc4437ec2a5549f54a29
oNEOBYTEo <andersoncifuentesdev@gmail.com>
2022-11-05 17:08:53 -0500

    security validations added

---
+++ utils/jwt.ts
@@ -0,0 +1,38 @@
+import jwt from 'jsonwebtoken';
+
+export const signToken = (_id: string, email: string) => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+  }
+
+  return jwt.sign(
+    // Payload
+    { _id, email },
+
+    // Seed
+    process.env.JWT_SECRET_SEED,
+
+    // Options
+    { expiresIn: '30d' }
+  );
+};
+
+export const isValidToken = (token: string): Promise<string> => {
+  if (!process.env.JWT_SECRET_SEED) {
+    throw new Error('No hay semilla de JWT - Revisar variables de entorno');
+  }
+
+  return new Promise((resolve, reject) => {
+    try {
+      jwt.verify(token, process.env.JWT_SECRET_SEED || '', (err, payload) => {
+        if (err) return reject('JWT no es válido');
+
+        const { _id } = payload as { _id: string };
+
+        resolve(_id);
+      });
+    } catch (error) {
+      return reject('JWT no es válido');
+    }
+  });
+};
